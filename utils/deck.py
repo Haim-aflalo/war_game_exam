31c54eb45c26fae0f8156d74f58829a8a20a3556
Haim Aflalo <kodkodhaim@gmail.com>
2025-10-23 10:12:09 +0300

    function compare_card created

--- utils/deck.py
+++ utils/deck.py
@@ -6,11 +6,18 @@ def create_card(rank:str,suite:str) -> dict:
         value = val_high_card[rank]
     return {"rank":rank,"suite":suite,"value":value}
 
-# def compare_cards(p1_card:dict, p2_card:dict) -> str:
-#     return "g"
-#
+def compare_cards(p1_card:dict, p2_card:dict) -> str:
+    if p1_card["value"] > p2_card["value"]:
+        return "p1"
+    if p1_card["value"]< p2_card["value"]:
+        return  "p2"
+    else:
+        return "WAR"
+
+
 # def create_deck() -> list[dict]:
 #     return []
 #
 # def shuffle(deck: list[dict]) -> list[dict]:
 #     return  []
+
