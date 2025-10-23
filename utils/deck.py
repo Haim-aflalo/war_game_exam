303acf2926f0eef8661c352b25c3147eb160a85d
Haim Aflalo <kodkodhaim@gmail.com>
2025-10-23 10:45:54 +0300

    the function create deck is created and correction on create card

--- utils/deck.py
+++ utils/deck.py
@@ -1,6 +1,7 @@
+
 def create_card(rank:str,suite:str) -> dict:
     val_high_card = {"J":11,"Q":12,"K":13,"A":14}
-    if isinstance(rank,int):
+    if rank.isdigit():
         value = int(rank)
     else:
         value = val_high_card[rank]
@@ -15,9 +16,62 @@ def compare_cards(p1_card:dict, p2_card:dict) -> str:
         return "WAR"
 
 
-# def create_deck() -> list[dict]:
-#     return []
-#
+def create_deck() -> list[dict]:
+    deck = []
+    for i in range(2,15):
+        if i <= 10:
+            deck.append(create_card(str(i),"S"))
+        else:
+            if i == 11:
+                deck.append(create_card("J", "S"))
+            elif i == 12:
+                deck.append(create_card("Q", "S"))
+            elif i == 13:
+                9
+                deck.append(create_card("K", "S"))
+            elif i == 14:
+                deck.append(create_card("A", "S"))
+
+    for i in range(2,15):
+        if i <= 10:
+            deck.append(create_card(str(i),"H"))
+        else:
+            if i == 11:
+                deck.append(create_card("J", "H"))
+            elif i == 12:
+                deck.append(create_card("Q", "H"))
+            elif i == 13:
+                deck.append(create_card("K", "H"))
+            elif i == 14:
+                deck.append(create_card("A", "H"))
+
+    for i in range(2,15):
+        if i <= 10:
+            deck.append(create_card(str(i),"C"))
+        else:
+            if i == 11:
+                deck.append(create_card("J", "C"))
+            elif i == 12:
+                deck.append(create_card("Q", "C"))
+            elif i == 13:
+                deck.append(create_card("K", "C"))
+            elif i == 14:
+                deck.append(create_card("A", "C"))
+    for i in range(2,15):
+        if i <= 10:
+            deck.append(create_card(str(i),"D"))
+        else:
+            if i == 11:
+                deck.append(create_card("J", "D"))
+            elif i == 12:
+                deck.append(create_card("Q", "D"))
+            elif i == 13:
+                deck.append(create_card("K", "D"))
+            elif i == 14:
+                deck.append(create_card("A", "D"))
+    return deck
+
 # def shuffle(deck: list[dict]) -> list[dict]:
 #     return  []
+print(create_deck())
 
