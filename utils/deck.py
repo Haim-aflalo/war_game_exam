dc34562b40f1686bcd0cac4bd4b26d7b087dbe08
Haim Aflalo <kodkodhaim@gmail.com>
2025-10-23 09:55:59 +0300

    function create_card created

--- utils/deck.py
+++ utils/deck.py
@@ -1,11 +1,16 @@
 def create_card(rank:str,suite:str) -> dict:
-    return {}
+    val_high_card = {"J":11,"Q":12,"K":13,"A":14}
+    if isinstance(rank,int):
+        value = int(rank)
+    else:
+        value = val_high_card[rank]
+    return {"rank":rank,"suite":suite,"value":value}
 
-def compare_cards(p1_card:dict, p2_card:dict) -> str:
-    return "g"
-
-def create_deck() -> list[dict]:
-    return []
-
-def shuffle(deck: list[dict]) -> list[dict]:
-    return  []
+# def compare_cards(p1_card:dict, p2_card:dict) -> str:
+#     return "g"
+#
+# def create_deck() -> list[dict]:
+#     return []
+#
+# def shuffle(deck: list[dict]) -> list[dict]:
+#     return  []
